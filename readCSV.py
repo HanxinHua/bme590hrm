221f48b1fd906d348a261c38d3aa795b6e907473
HanxinHua <HanxinHua@github.com>
2018-10-23 14:29:17 -0400

    finish read

---
+++ readCSV.py
@@ -0,0 +1,17 @@
+import csv
+from Is_Number import isNumber
+
+def readCsv():
+    path = "test_data/test_data31.csv"
+    with open(path, newline='') as f:
+        data = list(csv.reader(f))
+    for item in data:
+        if(not isNumber(item[0]) or (not isNumber(item[1]))):
+            print(item)
+            data.remove(item)
+        else:
+            item[0] = float(item[0])
+            item[1] = float(item[1])
+            if (item[1]>300) :
+                data.remove(item)
+    return data
