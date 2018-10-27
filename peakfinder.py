bc90588d755c1d2b2262d9f0630266860d8083fc
HanxinHua <HanxinHua@github.com>
2018-10-26 20:37:25 -0400

    peakfinder finished

--- peakfinder.py
+++ peakfinder.py
@@ -2,11 +2,6 @@ import numpy as np
 import peakutils
 
 
-def peakfinder(data):
+def peak_finder(data):
     ecg = peakutils.indexes(np.array(data), thres=0.02/max(data), min_dist=200)
-    return ecg
-
-
-def valleyfinder(data):
-    ecg = peakutils.indexes(-np.array(data), thres=0.02/max(data), min_dist=200)
     return ecg
