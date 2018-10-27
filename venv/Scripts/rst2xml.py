6b2af8a19a58b71f6c078c7648ec362987c2e8c5
HanxinHua <HanxinHua@github.com>
2018-10-27 14:45:10 -0400

    build sphinx

---
+++ venv/Scripts/rst2xml.py
@@ -0,0 +1,4 @@
+#!c:\users\hh177\desktop\medica~1\bme590~1\venv\scripts\python.exe
+# EASY-INSTALL-SCRIPT: 'docutils==0.14','rst2xml.py'
+__requires__ = 'docutils==0.14'
+__import__('pkg_resources').run_script('docutils==0.14', 'rst2xml.py')
---
+++ venv/Scripts/sphinx-build-script.py
@@ -0,0 +1,12 @@
+#!c:\users\hh177\desktop\medica~1\bme590~1\venv\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Sphinx==1.8.1','console_scripts','sphinx-build'
+__requires__ = 'Sphinx==1.8.1'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('Sphinx==1.8.1', 'console_scripts', 'sphinx-build')()
+    )
